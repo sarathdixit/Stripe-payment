750d2135376a364ede657227c66e2dbcbcd1a7ad
sarathdixit <sarathdixit29@gmail.com>
2020-08-24 22:57:55 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,70 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+ 
+import { StripeService, StripeCardComponent } from 'ngx-stripe';
+import {
+  StripeCardElementOptions,
+  StripeElementsOptions
+} from '@stripe/stripe-js';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'stripeintegration';
+   @ViewChild(StripeCardComponent) card: StripeCardComponent;
+ 
+  cardOptions: StripeCardElementOptions = {
+    style: {
+      base: {
+        iconColor: '#666EE8',
+        color: '#31325F',
+        fontWeight: '300',
+        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+        fontSize: '18px',
+        '::placeholder': {
+          color: '#CFD7E0'
+        }
+      }
+    }
+  };
+ 
+  elementsOptions: StripeElementsOptions = {
+    locale: 'en'
+  };
+ 
+  stripeTest: FormGroup;
+ 
+  constructor(private fb: FormBuilder, private stripeService: StripeService) {}
+ 
+  ngOnInit(): void {
+    this.stripeTest = this.fb.group({
+      name: ['', [Validators.required]]
+    });
+  }
+ 
+  createToken(): void {
+    const name = this.stripeTest.get('name').value;
+    let clientSecret = "pi_1HJj93I24F1DWpOnF5gwAooY_secret_F0AYvoDGiub45nEv2HhJ7qL0M"
+    this.stripeService.confirmCardPayment(clientSecret, {
+      payment_method: {
+        card: this.card.element,
+        billing_details: {
+          name: name
+        }
+      },
+      setup_future_usage: 'off_session'
+    }).subscribe( data => console.log(data), err => console.log(err))
+    // this.stripeService
+    //   .createToken(this.card.element, { name })
+    //   .subscribe((result) => {
+    //     if (result.token) {
+    //       // Use the token
+    //       console.log(result.token.id);
+    //     } else if (result.error) {
+    //       // Error creating the token
+    //       console.log(result.error.message);
+    //     }
+    //   });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// Import the library
+import { NgxStripeModule } from 'ngx-stripe';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    NgxStripeModule.forRoot('pk_test_51HJbU2I24F1DWpOn39zVdmcTrFWEFdfjFRn28FQ9omhfRJII0tjRdXRy44l9GNCKsrxxffcO20kn9KSs0T4RRfXB00mI1tJP0Y'),
   ],
   providers: [],
   bootstrap: [AppComponent]
